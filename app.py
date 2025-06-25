9b2f44de4de4f984d2241f67d625831e581eae97
Gaurav Singh <141561295+gaurav-singh2004@users.noreply.github.com>
2025-06-26 01:45:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,21 +52,15 @@
 import streamlit as st
 import pickle 
 import string
-# import os
-# import nltk 
 import nltk
 import os
 nltk.data.path.append(os.path.join(os.getcwd(), "nltk_data"))
 
-nltk.download('punkt', download_dir=os.path.join(os.getcwd(), 'nltk_data'))
-nltk.download('stopwords', download_dir=os.path.join(os.getcwd(), 'nltk_data'))
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('stopwords')
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-# nltk.data.path.append(os.path.join(os.getcwd(), "nltk_data"))
 
 # Minimal styling using markdown
 st.markdown("""
